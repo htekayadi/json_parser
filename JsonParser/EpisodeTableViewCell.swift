74bc6940ba2ee7445d839f71e68bcd6de3bdfaab
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-01 23:06:01 +0800

    Add view EpisodeTableViewCell

---
+++ JsonParser/EpisodeTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  EpisodeTableViewCell.swift
+//  JsonParser
+//
+//  Created by Popsical on 1/5/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+    
+    func updateUI() {
+        
+    }
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var createdAtLabel: UILabel!
+    @IBOutlet weak var authorImageView: UIImageView!
+}
